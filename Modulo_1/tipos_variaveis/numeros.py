8c7ce27ccd398d2dcd2db25ec24ce763775e4c05
Cruz <douglas.mgs4@gmail.com>
2024-02-27 23:57:34 -0300

    small changes

---
+++ Modulo_1/tipos_variaveis/numeros.py
@@ -0,0 +1,43 @@
+#Inteiro
+numero_inteiro = 7
+print("Inteiro = ", numero_inteiro)
+
+#Real com ponto flutuante
+numero_real = 1.99
+print("Real com ponto flutuante = ", numero_real)
+
+#type()
+print("Tipo da variável inteiro = ", type(numero_inteiro))
+print("Tipo da variável real = ", type(numero_real))
+
+# soma +
+soma = 1 + 1
+print("1 + 1 = ", soma)
+
+# subtração -
+subtracao = 1 - 1
+print("1 - 1 =", subtracao)
+
+#multiplicação *
+multiplicacao = 2 * 2
+print("2 x 2 = ", multiplicacao)
+
+# divisao /
+divisao = 5 / 2
+print("5 / 2 = ", divisao)
+print("Tipo da variável do resultado da divisão = ", type(divisao))
+
+#int
+print("Valor em inteiro =", int(divisao))
+
+#float ()
+print("Valor em float = ", float(divisao))
+
+#modulo %
+modulo = 5 % 2
+print("Módulo = ", modulo)
+
+#divisao inteiro //
+divisao = 5 // 2
+print("5 // 2 = ", divisao)
+print("Tipo da variável do resultado da divisao = ", type(divisao))
